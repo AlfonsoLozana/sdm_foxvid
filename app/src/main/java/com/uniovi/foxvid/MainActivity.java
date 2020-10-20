c88b718a0326c7c522828a8a64f4b9bc243b905e
AlfonsoLozana <uo264354@uniovi.es>
2020-10-20 12:02:25 +0200

    Anadiendo un post

--- app/src/main/java/com/uniovi/foxvid/MainActivity.java
+++ app/src/main/java/com/uniovi/foxvid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uniovi.foxvid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -10,12 +11,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btLogOut;
+    private Button btPost;
+
+    private TextView txtPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +36,19 @@ public class MainActivity extends AppCompatActivity {
 
 
         btLogOut = (Button)findViewById(R.id.idLogOut);
+        btPost = (Button)findViewById(R.id.idBtPost);
+        txtPost = (TextView)findViewById(R.id.idTxtPost);
+
         btLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 logOut();
+            }
+        });
+        btPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                post();
             }
         });
 
@@ -46,6 +67,32 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,Login.class);
         startActivity(intent);
         finish();
+    }
+
+    private void post(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String uuid = UUID.randomUUID().toString();
+        Map<String, String> posts = new HashMap<>();
+        posts.put("uid",uuid );
+        posts.put("post", txtPost.getText().toString());
+        posts.put("user", FirebaseAuth.getInstance().getCurrentUser().getUid());
+        posts.put("fecha", Timestamp.now().toString());
+
+
+        db.collection("post").document(uuid)
+                .set(posts)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 
 }
