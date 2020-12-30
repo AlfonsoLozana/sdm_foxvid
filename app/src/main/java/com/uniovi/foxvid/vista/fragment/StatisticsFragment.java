7e45a7f8b1c00541e9b00c1996ba7f07342fad29
Mario-Miguel <mariomig5@gmail.com>
2020-12-30 11:39:52 +0100

    Mapa añadido

--- app/src/main/java/com/uniovi/foxvid/vista/fragment/StatisticsFragment.java
+++ app/src/main/java/com/uniovi/foxvid/vista/fragment/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package com.uniovi.foxvid.vista.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,24 +9,66 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.uniovi.foxvid.R;
 import com.uniovi.foxvid.modelo.Coordinate;
 
-public class StatisticsFragment extends Fragment {
+public class StatisticsFragment extends Fragment implements OnMapReadyCallback {
 
-    private Button btPost;
-    private TextView txtPost;
     private Coordinate coordinate;
+    private View root;
+    SupportMapFragment mapFragment;
+
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View root =  inflater.inflate(R.layout.fragment_statistics, container, false);
+        root =  inflater.inflate(R.layout.fragment_statistics, container, false);
 
         return root;
     }
+
+
+
+    private void initMap() {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     *
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 
 }
