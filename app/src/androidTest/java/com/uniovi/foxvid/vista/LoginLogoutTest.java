8192031cf454ff50202bfef2bc0bc955b10bc864
Mario-Miguel <mariomig5@gmail.com>
2021-01-09 11:56:44 +0100

    Primeros tests con espresso

---
+++ app/src/androidTest/java/com/uniovi/foxvid/vista/LoginLogoutTest.java
@@ -0,0 +1,139 @@
+package com.uniovi.foxvid.vista;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.uniovi.foxvid.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginLogoutTest {
+
+    @Rule
+    public ActivityTestRule<Login> mActivityTestRule = new ActivityTestRule<>(Login.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION");
+
+    @Test
+    public void loginLogoutTest() {
+        ViewInteraction button = onView(
+                allOf(withId(R.id.btGoogle), withText("GOOGLE"),
+                        withParent(withParent(withId(R.id.idLoginLayaut))),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.btGithub), withText("GITHUB"),
+                        withParent(withParent(withId(R.id.idLoginLayaut))),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btGoogle), withText("Google"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.idLoginLayaut),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.btProfile),
+                        withParent(withParent(withId(R.id.appBarLayout))),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.btProfile),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.appBarLayout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btnLogOut), withText("Cerrar sesión"),
+                        childAtPosition(
+                                allOf(withId(R.id.botonera),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                3)),
+                                0),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.btGoogle), withText("GOOGLE"),
+                        withParent(withParent(withId(R.id.idLoginLayaut))),
+                        isDisplayed()));
+        button3.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btGithub), withText("Github"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.idLoginLayaut),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction viewGroup = onView(
+                allOf(withId(R.id.idLoginLayaut),
+                        withParent(allOf(withId(android.R.id.content),
+                                withParent(withId(R.id.action_bar_root)))),
+                        isDisplayed()));
+        viewGroup.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
